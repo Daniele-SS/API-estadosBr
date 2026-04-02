d42bd3b5282715e3902e0633eb256a4efba87cba
Daniele-SS <dev.daniele@outlook.com>
2026-04-02 10:42:21 -0300

    feat: API finalizada

---
+++ exercicio_06/app.js
@@ -0,0 +1,165 @@
+/************************************************************************************
+ * Objetivo: Arquivo responsável pela criação da API do projeto de Estados e Cidades
+ * Data: 01/04/2026
+ * Autora: Daniele Silva Santos
+ * Versão: 1.0
+ ************************************************************************************/
+
+/**
+ *  *   Instalação do EXPRESS - npm install express --save
+ *        Dependência responsável pela utilização do protocolo HTTP para criar uma API
+ * 
+ * 
+ *      Instalação do CORS - npm install cors --save
+ *        Depedência resposável pelas configurações a serem realizadas para a permissão de acesso da API
+ */
+
+//Import das depedências para criar a API
+const express   = require ('express')
+const cors      = require ('cors')
+
+const app = express() //Criando um objeto para manipular o express
+
+//Conjunto de permissões a serem aplicadas no cors da API
+const corsOptions = {
+    origin: ['*'], //A origem da requisição, podendo ser um IP ou um asterisco '*' (que significa todos)
+    methods: 'GET', //São os verbos que serão liberados na API (GET, POST, PUT e DELETE)
+    allowedHeaders: ['Content-type', 'Autorization'] //São permissões de cabeçalho do cors
+}
+
+app.use(cors(corsOptions)) //Configura as permissões da API através do cors
+
+
+//Response -> Retornos da API
+//Request -> Chgedas de dados na API
+
+//Import do arquivo de funções
+const estadosCidades = require ('./modulo/functions')
+
+//Retorna dados dos estados filtrando pelo UF
+app.get('/v1/senai/dados/estado/:uf', function(request, response){
+
+    let sigla   = request.params.uf
+    let estado  = estadosCidades.getDadosEstado(sigla)
+
+    if(estado) {
+    response.status(200)
+    response.json(estado)
+    } else {
+        response.status(404)
+        response.json({"message": "O estado informado não foi encontrado!"})
+    }
+})
+
+
+//Retorna dados da capital de cada estado filtrando pelo UF
+app.get('/v1/senai/capital/estado/:uf', function(request, response) {
+    let sigla           = request.params.uf
+    let capitalEstado   = estadosCidades.getCapitalEstado(sigla)
+
+    if(capitalEstado) {
+        response.status(200)
+        response.json(capitalEstado)
+
+    } else {
+        response.status(404)
+        response.json({"message": "ERROR: Estado fornecido não existe!"})
+    }
+
+})
+
+
+//Retorna dados dos estados filtrando pela região
+app.get('/v1/senai/estados/regiao/:regiao', function(request, response){
+    let regiao          = request.params.regiao
+    let regiaoEstado    = estadosCidades.getEstadosRegiao(regiao)
+
+    if(regiaoEstado) {
+        response.status(200)
+        response.json(regiaoEstado)
+
+    } else {
+        response.status(404)
+        response.json({"message": "ERROR: A região fornecida é inválida!"})
+    }
+})
+
+
+//Retorna dados dos estados que foram capitais do Brasil
+app.get('/v1/senai/estados/capital/brasil', function (request, response){
+    let siglaCapital    = request.params
+    let capitais        = estadosCidades.getCapitalPais(siglaCapital)
+
+    response.status(200)
+    response.json(capitais)
+})
+
+
+//Retorna dados da cidades filtrando pela UF
+app.get('/v1/senai/estados/cidade/:uf', function(request, response){
+    let cidade      = request.params.uf
+    let cidadesBr   = estadosCidades.getCidades(cidade)
+
+    if(cidadesBr) {
+        response.status(200)
+        response.json(cidadesBr)
+
+    } else {
+        response.status(404)
+        response.json({"message": "ERROR: Estado fornecido não existe!"})
+    }
+})
+
+
+//Retorna dados das cidades filtrando pela UF
+app.get('/v1/senai/estados', function(request, response) {
+
+    let estados = estadosCidades.getListaDeEstados() //Chama a função que retorna a lista de estados
+
+    response.status(200)
+    response.json(estados)
+})
+
+
+app.get('/v1/senai/help', function(request, response){
+    let docAPI = {
+        "API-description": "API para manipular dados de Estados e Cidades",
+        "Date": "2026-04-02",
+        "Developer": "Daniele Silva Santos",
+        "Version": "1.0",
+        "Endpoints": [
+            {   "id": 1,
+                "Rota 1": "/v1/senai/estados",
+                "OBS": "Retorna a lista de todos os estados brasileiros",
+            },
+            {   "id": 2,
+                "Rota 2": "/v1/senai/dados/estado/al",
+                "OBS": "Retorna os dados do estado filtrando pela sigla do estado"
+            },
+            {   "id": 3,
+                "Rota 3": "/v1/senai/capital/estado/se",
+                "OBS": "Retorna os dados da capital filtrando pela sigla do estado"
+            },
+            {   "id": 4,
+                "Rota 4": "/v1/senai/estados/regiao/centro-oeste",
+                "OBS": "Retorna todos os estados referente a uma região"
+            },
+            {   "id": 5,
+                "Rota 5": "/v1/senai/estados/capital/brasil",
+                "OBS": "Retorna todos os estados que formaram capital do Brasil"
+            },
+            {   "id": 6,
+                "Rota 6": "/v1/senai/estados/cidade/sp",
+                "OBS": "Retorna todas as cidades filtrando pela sigla do estado"
+            }
+        ]
+    }
+
+    response.status(200)
+    response.json(docAPI)
+})
+
+//Serve para inicializar a API para receber requisições (8080 é minha porta para receber a requisição)
+app.listen(8080, function(){
+    console.log('API funcionando e aguardando novas requisições ...')
+})
---
+++ exercicio_06/modulo/functions.js
@@ -0,0 +1,160 @@
+/****************************************
+ * Objetivo: Obter uma lista de estados
+ * Autora: Daniele Silva Santos
+ * Data: 18/03/2026
+ * Versão: 1.0
+ ****************************************/
+
+const listaDeEstados = require('./estados_cidades.js')
+
+const getListaDeEstados = function () {
+    let estados = []
+
+    listaDeEstados.estados.forEach(function(estado) {
+        estados.push(estado.sigla)
+    })
+
+    return {
+        uf: estados,
+        quantidade: estados.length//Retorna a quantidade de estados
+    }
+}
+    
+const getDadosEstado = function (uf) {
+    let resultado
+    let retornoFalso = false //return false da variável caso seja fornecido um estado que não existe
+
+    listaDeEstados.estados.forEach(function(descricao) {
+        if (descricao.sigla.toLocaleUpperCase() === uf.toLocaleUpperCase()) {
+                resultado = {
+                    uf: descricao.sigla,
+                    descricao: descricao.nome,
+                    capital: descricao.capital,
+                    regiao: descricao.regiao
+
+                }//Fechamento do JSON
+
+                retornoFalso = true
+
+        }//Fechamento do if
+
+    })
+
+    if (retornoFalso) {
+        return resultado
+
+    } else {
+        return false
+    }
+}
+
+const getCapitalEstado = function (uf) {
+    let info 
+    let retornoFalso = false
+
+    listaDeEstados.estados.forEach(function(result) {
+        if (result.sigla.toUpperCase() === uf.toUpperCase()) {
+                info = {
+                uf: result.sigla,
+                descricao: result.nome,
+                capital: result.capital
+            }
+
+            retornoFalso = true
+
+        }
+    })
+
+    if (retornoFalso) {
+        return info
+
+    } else {
+        return false
+    }
+}
+
+const getEstadosRegiao = function (reg) {
+    let aux = []/*A variável vira um array porque uma região tem vários estados. 
+                Será preciso percorrer o array para localizar a região e seus estados */
+    let retornoFalso = false
+
+    listaDeEstados.estados.forEach(function(estado) {
+        if (estado.regiao.toUpperCase() === reg.toUpperCase()) {
+            aux.push({//O .push() é usado para poder 'puxar' todos os estados da região selecionada
+                uf: estado.sigla,
+                descricao: estado.nome
+            })
+
+            retornoFalso = true
+        }
+    })
+
+    
+    if (retornoFalso) {
+        return {
+            regiao: reg,
+            estados: aux
+        }
+
+    } else {
+        return false
+    }
+}
+
+const getCapitalPais = function () {
+    let capitais = []
+    let json = {}
+
+    listaDeEstados.estados.forEach(function(capitalBr) {
+        if (capitalBr.capital_pais) {
+            capitais.push({
+                capital_atual: capitalBr.capital_pais.capital,
+                uf: capitalBr.sigla,
+                descricao: capitalBr.nome,
+                capital: capitalBr.capital,
+                regiao: capitalBr.regiao,
+                capital_pais_ano_inicio: capitalBr.capital_pais.ano_inicio,
+                capital_pais_ano_termino: capitalBr.capital_pais.ano_fim
+            })
+        }
+    })
+
+    json.capitais = capitais
+
+    return json
+}
+
+const getCidades = function (siglaEstado) {
+    let estadoSelecionado = []
+    let retornoFalso = false
+    
+    listaDeEstados.estados.forEach(function(estado){
+        if (estado.sigla.toUpperCase() === siglaEstado.toUpperCase()) {
+            estadoSelecionado.push({
+                uf: estado.sigla,
+                descricao: estado.nome,
+                quantidade: estado.cidades.length,
+                cidades: estado.cidades.map(cidade => cidade.nome)
+            })
+
+            retornoFalso = true
+       
+        }
+    })
+
+    if (retornoFalso) {
+        return estadoSelecionado
+
+    } else {
+        return false
+    }
+}
+
+module.exports = {
+    getListaDeEstados,
+    getDadosEstado,
+    getCapitalEstado,
+    getEstadosRegiao,
+    getCapitalPais,
+    getCidades
+}
